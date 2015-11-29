18e3960d9ff30f9c2eba34aabf9893c7ee09e275
Hanse <hanse19@gmail.com>
2015-11-29 15:43:12 +0100

    Transform immutable objects for better logging

--- src/utils/configureStore.js
+++ src/utils/configureStore.js
@@ -2,13 +2,18 @@ import { compose, createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import { createHashHistory } from 'history';
+import { Iterable } from 'immutable';
 import { reduxReactRouter, routerStateReducer } from 'redux-router';
 import routes from '../routes';
 
 export default function configureStore(initialState) {
   const loggerMiddleware = createLogger({
     level: 'info',
-    collapsed: true
+    collapsed: true,
+    transformer: (state) => Object.keys(state).reduce((json, key) => {
+      json[key] = Iterable.isIterable(state[key]) ? state[key].toJS() : state[key];
+      return json;
+    }, {})
   });
 
   const middlewares = applyMiddleware(
