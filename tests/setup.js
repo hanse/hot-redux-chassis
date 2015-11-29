56ea48ba35d9b699f490e095037c77cd61f34db4
Hanse <hanse19@gmail.com>
2015-11-29 15:54:56 +0100

    exclude jpg in tests

--- tests/setup.js
+++ tests/setup.js
@@ -2,6 +2,7 @@ import { jsdom } from 'jsdom';
 
 require.extensions['.css'] = () => {};
 require.extensions['.png'] = () => {};
+require.extensions['.jpg'] = () => {};
 
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
