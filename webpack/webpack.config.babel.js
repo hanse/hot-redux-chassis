9b13cd6898b1dff641232e40fc4011eaed75ce24
Hanse <hkri@koren.im>
2016-09-22 14:21:58 +0200

    Upgrade  dependencies

--- webpack/webpack.config.babel.js
+++ webpack/webpack.config.babel.js
@@ -5,140 +5,136 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const packageJson = require('../package.json');
 
-const modules = [
-  path.resolve(__dirname, '../'),
-  'node_modules'
-];
+module.exports = (options) => ({
+  /**
+   * @see webpack-devtools
+   */
+  devtool: options.development && 'eval-source-map',
 
-module.exports = (options) => {
-  return {
-    /**
-     * @see webpack-devtools
-     */
-    devtool: options.development && 'eval-source-map',
-
-    /**
-     * Define webpack entries
-     */
-    entry: {
-      app: compact([
-        options.development && 'webpack-hot-middleware/client',
-        options.development && 'react-hot-loader/patch',
-        './app/index.js'
-      ]),
-      vendor: ['react', 'react-dom', 'lodash', 'react-router']
-    },
-
-    /**
-     * Define the output directory
-     */
-    output: {
-      path: path.join(__dirname, '../dist'),
-      filename: '[name].js',
-      chunkFilename: '[name].chunk.js',
-      publicPath: '/'
-    },
-
-    /**
-     *
-     */
-    plugins: compact([
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendor',
-        minChunks: Infinity,
-        filename: 'vendor.js'
-      }),
-
-      !options.development && new webpack.optimize.OccurrenceOrderPlugin(),
-
-      new webpack.DefinePlugin({
-        __DEV__: JSON.stringify(!!options.development),
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }),
-
-      options.development && new webpack.HotModuleReplacementPlugin(),
-      options.development && new webpack.NoErrorsPlugin(),
-
-      // Minify bundles
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        },
-        output: {
-          comments: false
-        }
-      }),
-
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      }),
-
-      // Make a separate css bundle for production
-      new ExtractTextPlugin({
-        filename: '[name].css',
-        allChunks: true,
-        disable: !!options.development
-      }),
-
-      // build a index.html with assets injected
-      new HtmlWebpackPlugin({
-        template: 'app/index.html',
-        inject: true,
-        hash: true,
-        favicon: 'app/assets/favicon.ico',
-        appName: packageJson.name
-      })
+  /**
+   * Define webpack entries
+   */
+  entry: {
+    app: compact([
+      options.development && 'webpack-hot-middleware/client',
+      options.development && 'react-hot-loader/patch',
+      './app/index.js'
     ]),
+    vendor: ['react', 'react-dom', 'lodash', 'react-router']
+  },
 
-    resolve: {
-      modules,
-      extensions: ['.js', '.json']
-    },
+  /**
+   * Define the output directory
+   */
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+    publicPath: '/'
+  },
 
-    module: {
-      loaders: [{
-        test: /\.jsx?$/,
-        loader: 'babel',
-        include: path.join(__dirname, '../app')
-      }, {
-        test: /\.css$/,
-        include: /node_modules/,
-        loaders: ['style', 'css']
-      }, {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        loaders: [
-          'style', {
-            loader: 'css',
-            query: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            }
-          },
-          'postcss'
-        ]
-      }, {
-        test: /\.json$/,
-        loader: 'json-loader'
-      }, {
-        test: /\.(png|jpg|mp4|webm)/,
-        loader: 'url',
-        query: {
-          limit: 8192
+  /**
+   *
+   */
+  plugins: compact([
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.js'
+    }),
+
+    !options.development && new webpack.optimize.OccurrenceOrderPlugin(),
+
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(!!options.development),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+
+    options.development && new webpack.HotModuleReplacementPlugin(),
+    options.development && new webpack.NoErrorsPlugin(),
+
+    // Minify bundles
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    }),
+
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        minimize: !options.development,
+        postcss(wp) {
+          return [
+            require('postcss-import')({ addDependencyTo: wp }),
+            require('postcss-cssnext'),
+            require('postcss-nested')
+          ];
         }
-      }]
-    },
+      }
+    }),
 
-    target: 'web',
+    // Make a separate css bundle for production
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      allChunks: true,
+      disable: !!options.development
+    }),
 
-    postcss(wp) {
-      return [
-        require('postcss-import')({ addDependencyTo: wp }),
-        require('postcss-cssnext'),
-        require('postcss-nested')
-      ];
-    }
-  };
-};
+    // build a index.html with assets injected
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+      inject: true,
+      hash: true,
+      favicon: 'app/assets/favicon.ico',
+      appName: packageJson.name
+    })
+  ]),
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, '../'),
+      'node_modules'
+    ],
+    extensions: ['.js', '.json']
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      include: path.join(__dirname, '../app')
+    }, {
+      test: /\.css$/,
+      include: /node_modules/,
+      loaders: ['style', 'css']
+    }, {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      loaders: [
+        'style', {
+          loader: 'css',
+          query: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: '[name]__[local]___[hash:base64:5]'
+          }
+        },
+        'postcss'
+      ]
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }, {
+      test: /\.(png|jpg|mp4|webm)/,
+      loader: 'url',
+      query: {
+        limit: 8192
+      }
+    }]
+  },
+
+  target: 'web'
+});
