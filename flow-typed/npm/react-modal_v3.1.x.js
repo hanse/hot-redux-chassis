5a2a46d1981bd28f6ba2472a59c50a384a814c15
Hanse <hkri@koren.im>
2018-06-14 19:34:36 +0200

    Upgrade flow-typed definitions

--- flow-typed/npm/react-modal_v3.1.x.js
+++ flow-typed/npm/react-modal_v3.1.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: 919d09445f69c0a85f337d1cdb035dc3
-// flow-typed version: b63741deb2/react-modal_v3.1.x/flow_>=v0.54.1
+// flow-typed signature: 1473e903c62ae7c73bdfc56d14576c15
+// flow-typed version: 80987ae0fa/react-modal_v3.1.x/flow_>=v0.54.1
 
 declare module 'react-modal' {
   declare type DefaultProps = {
@@ -35,7 +35,7 @@ declare module 'react-modal' {
       beforeClose: string
     },
     appElement?: HTMLElement | string | null,
-    onAfterOpen?: () => void,
+    onAfterOpen?: () => void | Promise<void>,
     onRequestClose?: () => void,
     aria?: {
       [key: string]: string
