89db0a40bf2f45e8482b4d29f18f6c04b438b265
Hans-Kristian Koren <hkri@koren.im>
2018-09-25 10:05:54 +0200

    clean up eslint config

--- app/types.js
+++ app/types.js
@@ -116,9 +116,7 @@ export type Store = ReduxStore<State, Action>;
 
 export type GetState = () => State;
 
-export type Thunk<A> = (d: Dispatch, gs: GetState) => Promise<void> | void | A;
-
-export type Dispatch = ReduxDispatch<Action> & ((t: Thunk<*>) => Action);
+export type Dispatch = ReduxDispatch<Action>;
 
 export type Epic = (
   actions$: Observable<Action>,
